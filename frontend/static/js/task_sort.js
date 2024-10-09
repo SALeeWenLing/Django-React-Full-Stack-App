a0789b090ed2eb5d827b9c39a71434292210827e
wlee0060 <wlee0060@student.monash.edu>
2024-10-09 20:40:20 +0800

    Add .trivyignore

--- frontend/static/js/task_sort.js
+++
@@ -1,5 +0,0 @@
-function sortTasks() {
-  var sortBy = document.getElementById('sort-by').value;
-  var url = window.location.pathname + '?sort_by=' + sortBy;  // Build the URL using the current path
-  window.location.href = url;
-}
